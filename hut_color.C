81797e591e997c0b50ee125e612b2e775182f81e
Rajesh <64485708+rajeshprbishnoi@users.noreply.github.com>
2021-12-14 00:32:17 +0530

    Update hut_color.C

--- hut_color.C
+++ hut_color.C
@@ -85,98 +85,99 @@ void draw_line(int x1, int y1, int x2, int y2){
 
 void main(){
 	int gd = DETECT, gm;
-	initgraph(&gd, &gm, "C:\\TURBOC3\\BGI");
+	initgraph(&gd, &gm, "C:\\TC\\BGI");
 
-	 // First rectangle
+	 // front rectangle
 	draw_rectangle(150, 180, 250, 300);
-	// second rectangle
+	// side rectangle
 	draw_rectangle(250, 180, 420, 300);
-	// Door
+	// Door rectangle
 	draw_rectangle(180, 250, 220, 300);
-	// window
-	draw_rectangle(310, 220, 360, 260);
-
-
-	// Roof
+	// windows rectangle
+	draw_rectangle(280, 220, 390, 260);
+    draw_line(335, 220, 335, 260);
+    draw_circle(325, 240, 3);
+    draw_circle(345, 240, 3);
+    draw_line(325, 240, 345, 240);
+    
+	// Roof of hut
 	line(200, 100, 150, 180);
 	draw_line(200, 100, 250, 180);
 	draw_line(200, 100, 370, 100);
 	draw_line(370, 100, 420, 180);
-	//Roof lining
+	
+	//Roof sections
 	draw_line(225, 140, 395, 140);
 	draw_line(285, 100, 335, 180);
 	draw_line(243, 100, 293, 180);
 	draw_line(327, 100, 377, 180);
-
-	// front circle
+    
+    // ventilator
 	draw_circle(200, 150, 10);
 
-	// Front lining
-	draw_line(150, 180, 180, 250);
-	line(250, 180, 220, 250);
-
-	// gate
-	// vertical Lines
+    // front horizontal lines
+	draw_line(150, 200, 250, 200);
+	draw_line(150, 225, 250, 225);
+	// front angular lines
+	draw_line(150, 225, 180, 250);
+	line(250, 225, 220, 250);
+	
+	// door vertical lines
 	draw_line(199, 250, 199, 300);
 	draw_line(201, 250, 201, 300);
-	// Horizontal Lines
-	draw_line(180, 260, 220, 260);
-	draw_line(180, 262, 220, 262);
-	draw_line(180, 274, 220, 274);
-	draw_line(180, 276, 220, 276);
+	// door latch
 	draw_circle(207, 283, 3);
-	boundary_fill(207, 283, 3, 15);
-	draw_line(180, 290, 220, 290);
-	draw_line(180, 292, 220, 292);
-
-	// window gate
-	draw_line(310, 220, 325, 227);
-	draw_line(325, 227, 325,260);
-	draw_circle(318, 242, 3);
-	line(360, 220, 345, 227);
-	draw_line(345, 227, 345, 260);
-	draw_circle(352, 242, 3);
-
-	// Front Circle color fill
-	boundary_fill(200, 150, 3, 15);
-
-	// Window color fill
-	boundary_fill(315, 225, 6, 15);
-	boundary_fill(318, 242, 3, 15);
-	boundary_fill(358, 225, 6, 15);
-	boundary_fill(352, 242, 3, 15);
-
+	
+	// Side angular lines 
+	draw_line(250,180, 280, 220);
+	line(280, 260, 250, 300);
+	line(420, 180, 390, 220);
+	draw_line(390, 260, 420, 300);
+    
+    draw_rectangle(293, 180, 377, 200);
+    draw_rectangle(293, 280, 377, 300);
+    
 	// Front Up color fill
-	boundary_fill(200, 105, 5, 15);
-
-	// Door Up color fill
-	//boundary_fill(200, 190, 5, 15);
-
-	// Door sides color fill
-	boundary_fill(155, 195, 7, 15);
-	boundary_fill(245, 195, 7, 15);
-
+	boundary_fill(200, 105, 10, 15);
+    boundary_fill(200, 150, 7, 15);
+    
+    // Door up color fill 
+    boundary_fill(155, 185, 14, 15);
+    boundary_fill(155, 210, 12, 15);
+    boundary_fill(180, 230, 11, 15);
+    
+    // Door side color fill 
+    boundary_fill(160, 250, 9, 15);
+    boundary_fill(240, 250, 9, 15);
+    
+    // Door color fill
+    boundary_fill(185, 255, 6, 15);
+    boundary_fill(215, 255, 6, 15);
+    boundary_fill(207, 283, 7, 15);
+    
 	// Roof color fill
-	boundary_fill(210, 110, 6, 15);
-	boundary_fill(253, 110, 6, 15);
-	boundary_fill(295, 110, 6, 15);
-	boundary_fill(337, 110, 6, 15);
-	boundary_fill(235, 150, 6, 15);
-	boundary_fill(278, 150, 6, 15);
-	boundary_fill(328, 150, 6, 15);
-	boundary_fill(370, 150, 6, 15);
+	boundary_fill(210, 110, 1, 15);
+	boundary_fill(253, 110, 7, 15);
+	boundary_fill(295, 110, 1, 15);
+	boundary_fill(337, 110, 7, 15);
+	boundary_fill(235, 150, 7, 15);
+	boundary_fill(278, 150, 1, 15);
+	boundary_fill(328, 150, 7, 15);
+	boundary_fill(370, 150, 1, 15);
 
-	// GATE color fill
-	boundary_fill(185, 255, 13, 15);
-	boundary_fill(185, 265, 10, 15);
-	boundary_fill(185, 282, 12, 15);
-	boundary_fill(185, 295, 5, 15);
-	boundary_fill(210, 255, 13, 15);
-	boundary_fill(210, 265, 10, 15);
-	boundary_fill(210, 282, 12, 15);
-	boundary_fill(210, 295, 5, 15);
-
-
+    // windows Color fill 
+    boundary_fill(285, 225, 8, 15);
+    boundary_fill(340, 225, 8, 15);
+    
+    // side rectangle fill 
+    boundary_fill(260, 230, 5, 15);
+    boundary_fill(410, 230, 5, 15);
+    
+    boundary_fill(300, 190, 2, 15);
+    boundary_fill(300, 290, 2, 15);
+    
+    boundary_fill(270, 190, 14, 15);
+    boundary_fill(270, 290, 14, 15);
 	getch();
 	closegraph();
 }
